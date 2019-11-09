99e0a45f9aac84576ed2766dcf85d32dec6c1863
Mikulew <mikulew@gmail.com>
2019-11-09 12:18:35 +0100

    Adding routes.js, products.js etc.

--- boilerplate/serverMiddleware/routes.js
+++ boilerplate/serverMiddleware/routes.js
@@ -1,3 +1,19 @@
+const products = [
+  {
+    'id': '1',
+    'name': 'something 1'
+  },
+  {
+    'id': '2',
+    'name': 'something 2'
+  },
+  {
+    'id': '3',
+    'name': 'something 3'
+  }
+]
+
 export default (app) => {
   app.get(`/hello`, (_req, res) => res.status(200).end('no hej'))
+  app.post(`/products`, (req, res) => products.push({ name: req.body.name, id}))
 }
